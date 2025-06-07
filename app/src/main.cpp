7938340ce963a3c2d436d531577f408584a0f493
Mikhail Doronin <dexter.dopotop@gmail.com>
2025-06-07 11:35:23 +0300

    init commit

---
+++ app/src/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+extern "C" {
+#include "comp128.h"
+}
+
+int main()
+{
+    uint8_t ki[128];
+    uint8_t srand[128];
+    uint8_t sres[128];
+    uint8_t kc[128];
+    comp128(ki, srand, sres, kc);
+    std::cout << "hello\n";
+    return 0;
+}
